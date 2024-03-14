ce8e41817114dfeee6cf0a2a847118423a744227
TanjibQuad <tanjib@quadque.tech>
2024-03-14 12:29:39 +0600

    card details save to stripe

--- app/Http/Controllers/payment/CardDetailsController.php
+++ app/Http/Controllers/payment/CardDetailsController.php
@@ -45,6 +45,7 @@ class CardDetailsController extends Controller
         }
         $stripe_response = $stripeDetails->stripeCardCreate($card_data);
         // array_push($card_data,$stripe_response->id);
+        
         dd($stripe_response);
         $response = $insertCardDetails->saveCardDetails($card_data);
         if ($response) {
