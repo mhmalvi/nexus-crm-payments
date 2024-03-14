1058490af4764e6a5d3a8bba62a6ef540d7746bb
TanjibQuad <tanjib@quadque.tech>
2024-03-14 14:04:27 +0600

    card details save to stripe

--- app/Http/Controllers/payment/CardDetailsController.php
+++ app/Http/Controllers/payment/CardDetailsController.php
@@ -48,7 +48,7 @@ class CardDetailsController extends Controller
         // dd($stripe_response)
         $stripe_response = Http::withHeaders([
     'Content-Type'=>'application/x-www-form-urlencoded',
-    'Authorization'=> 'Bearer '.config("app.stripe_secret"),
+    'Authorization'=> config("app.stripe_secret"),
     'source'=>$card_data[5]
 ])->post("https://api.stripe.com/v1/customers/".$card_data[6]."/sources");
 dd($stripe_response->body());
