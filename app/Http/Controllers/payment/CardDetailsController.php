999d8c62d96d3e237d9a28c5f197282fc44a442a
TanjibQuad <tanjib@quadque.tech>
2024-03-14 11:18:10 +0600

    token generate

--- app/Http/Controllers/payment/CardDetailsController.php
+++ app/Http/Controllers/payment/CardDetailsController.php
@@ -37,7 +37,7 @@ class CardDetailsController extends Controller
             // $exp_date = $request->exp_date,
             // $cvc = $request->cvc,
         ];
-        // dd($card_data);
+        dd($card_data);
         $company = Company::where('client_id',$card_data[3])->where('admin',$card_data[4])->first();
         if($company){
             array_push($card_data,$company->connect_id);
