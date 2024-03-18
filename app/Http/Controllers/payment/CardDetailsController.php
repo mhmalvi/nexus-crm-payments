800307a7080831f6440cc72e999b77559887f801
TanjibQuad <tanjib@quadque.tech>
2024-03-18 10:02:54 +0600

    card details save to stripe

--- app/Http/Controllers/payment/CardDetailsController.php
+++ app/Http/Controllers/payment/CardDetailsController.php
@@ -54,7 +54,7 @@ class CardDetailsController extends Controller
     // 'source' => $card_data[5],
 // ]);
 $token = json_encode($card_data[5]);
-dd($token);
+// dd($token);
         $stripe = new \Stripe\StripeClient(config("app.stripe_secret"));
 $stripe->customers->createSource($card_data[6], ['source' => $token]);
         dd($stripe);
