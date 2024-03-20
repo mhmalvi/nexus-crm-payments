fa9db0ef7c9f096f46a759af2c15dafee4a6bbdb
TanjibQuad <tanjib@quadque.tech>
2024-03-20 10:38:13 +0600

    create subscription

--- app/Http/Controllers/subscription/SubscriptionController.php
+++ app/Http/Controllers/subscription/SubscriptionController.php
@@ -30,7 +30,7 @@ class SubscriptionController extends Controller
             ], 500);
             }else{
                 $response = $this->createSubscriptions->createSubscription($request->customer_id);
-                dd(json_decode($response));
+                dd($response);
         if ($response) {
             return response()->json([
                 'message' => 'success',
