0fe47f70be9ca15c4d71a7dd0003a7b9cbb04e12
TanjibQuad <tanjib@quadque.tech>
2024-03-20 12:41:00 +0600

    get all transactions

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\payment\CardDetailsController;
-use App\Http\Controllers\payment\CheckoutPaymentController;
-use App\Http\Controllers\subscription\SubscriptionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\payment\CardDetailsController;
+use App\Http\Controllers\payment\CheckoutPaymentController;
+use App\Http\Controllers\transaction\TransactionController;
+use App\Http\Controllers\subscription\SubscriptionController;
 
 /*
 |--------------------------------------------------------------------------
