22fa8a7be7d8d36015b64eb52bcb2211159f5cf1
TanjibQuad <tanjib@quadque.tech>
2024-03-20 12:46:36 +0600

    get all transactions

--- app/Services/stripe/GetCustomerTransactionsService.php
+++ app/Services/stripe/GetCustomerTransactionsService.php
@@ -6,6 +6,6 @@ class GetCustomerTransactionsService{
     public function getCustomerTransactions($customerId){
         $stripe = new
         \Stripe\StripeClient(config("app.stripe_secret"));
-        return $stripe->customers->allBalanceTransactions($customerId);
+        return $stripe->customers->allBalanceTransactions($customerId,['limit'=>3]);
     }
 }
