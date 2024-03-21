591d493116f9eb9d7da654dcb4ed602091b2c68e
TanjibQuad <tanjib@quadque.tech>
2024-03-21 12:19:58 +0600

    create subscription modification

--- app/Services/stripe/CreateMonthlySubscriptionService.php
+++ app/Services/stripe/CreateMonthlySubscriptionService.php
@@ -23,6 +23,7 @@ class CreateMonthlySubscriptionService
 
             'items' => [['price' => $data[3]]],
         ]);
+        dd($response->data->id);
         $company->subscription_id = $response->data->id;
         $company->save();
         return $response;
