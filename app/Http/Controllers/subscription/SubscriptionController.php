b9018cfacc56e93074ec5c423909bad96afdc0db
TanjibQuad <tanjib@quadque.tech>
2024-03-24 13:09:41 +0600

    trial check

--- app/Http/Controllers/subscription/SubscriptionController.php
+++ app/Http/Controllers/subscription/SubscriptionController.php
@@ -98,8 +98,8 @@ class SubscriptionController extends Controller
             // $result = Carbon::createFromFormat('d/m/Y H:i:s',$company->end_date);
             $date = $company->end_date;
             $date = Carbon::parse($date)->subDays(3);
-            dd($date);
-            if(Carbon::now()>$date){
+            // dd($date);
+            if(Carbon::now()==$date){
                 dd('true');
             }else{
                 dd('false');
