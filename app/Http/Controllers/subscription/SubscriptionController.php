38c747ca6579af7a83a21737985809cb4da8908e
TanjibQuad <tanjib@quadque.tech>
2024-03-31 10:51:22 +0600

    latest

--- app/Http/Controllers/subscription/SubscriptionController.php
+++ app/Http/Controllers/subscription/SubscriptionController.php
@@ -73,7 +73,7 @@ class SubscriptionController extends Controller
                         array_push($data,$s_id->items->data[0]['id']);
                         $response = $this->upgradeSubscriptions->upgradeSubscription($data);
                     }
-                    dd($request->interval);
+                    dd($company->package_name);
                     if ($request->interval == "day" && $company->package_name == 'trial') {
                         // dd('day');
                         // dd($data);
