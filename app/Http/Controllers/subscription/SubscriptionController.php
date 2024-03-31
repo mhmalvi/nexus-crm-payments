b688ab7c5de4a5a9978e0e8c88c1b61bf1c8ac1a
TanjibQuad <tanjib@quadque.tech>
2024-03-31 10:30:41 +0600

    latest

--- app/Http/Controllers/subscription/SubscriptionController.php
+++ app/Http/Controllers/subscription/SubscriptionController.php
@@ -63,22 +63,20 @@ class SubscriptionController extends Controller
                         $price_id = $request->price_id,
                         $active = 1,
                     ];
-                    // dd($data);
+                    dd($data);
                     $response = "";
                     if($company->interval == 'day' && $request->interval == 'year'){
                         // dd('upgrade');
                         array_push($data,$request->sub_id);
                         $s_id = $this->retrieveSubscription->retrieveSubscription($request->sub_id);
                         // dd($s_id->items->data[0]['id']);
-
                         array_push($data,$s_id->items->data[0]['id']);
                         $response = $this->upgradeSubscriptions->upgradeSubscription($data);
                     }else if ($request->interval == "day" && $company->package_name == 'trial') {
                         // dd('day');
                         $response = $this->createMonthlySubscriptions->createSubscription($data);
                         // dd($response);
-                    } else if ($request->interval == "year" && $company->interval != 'day') {
-                        
+                    } else if ($request->interval == "year" && $company->interval != 'day') {                        
                         $response = $this->createYearlySubscriptions->createSubscription($data);
                         // dd($response);
                     }
