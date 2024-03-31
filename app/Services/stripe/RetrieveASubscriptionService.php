6fa1b4ae400078f5aebf24ac7cf5645caa5f6ca5
TanjibQuad <tanjib@quadque.tech>
2024-03-31 11:03:06 +0600

    latest

--- app/Services/stripe/RetrieveASubscriptionService.php
+++ app/Services/stripe/RetrieveASubscriptionService.php
@@ -4,7 +4,7 @@ namespace App\Services\stripe;
 
 class RetrieveASubscriptionService{
     public function retrieveSubscription($data){
-        dd($data);
+        // dd($data);
         $stripe = new
         \Stripe\StripeClient(config("app.stripe_secret"));
         return $stripe->subscriptions->retrieve($data, []);
