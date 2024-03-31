0d24048a47db8f104141c0bac7cf7c6c94580610
TanjibQuad <tanjib@quadque.tech>
2024-03-31 12:51:15 +0600

    latest

--- app/Services/stripe/UpgradeSubscriptionService.php
+++ app/Services/stripe/UpgradeSubscriptionService.php
@@ -2,18 +2,31 @@
 
 namespace App\Services\stripe;
 
+use Carbon\Carbon;
+use App\Models\Company;
+
 class UpgradeSubscriptionService
 {
     public function upgradeSubscription($data)
     {
         // dd($data);
+        $current_date = Carbon::now();
+        $end_date = $current_date->addDays(365);
+        $company = Company::where('connect_id',$data[0])->first();
+        $company->package = $data[2];
+        $company->interval = $data[1];
+        $company->end_date = $end_date;
+        $company->save();
         $stripe = new
             \Stripe\StripeClient(config("app.stripe_secret"));
-        return $stripe->subscriptions->update(
+        $response = $stripe->subscriptions->update(
             $data[5],
             ['items' => [[
                 'id'=>$data[5],
                 'price' => $data[3]]],]
         );
+        $company->subscription_id = $response->id;
+        $company->save();
+        return $response;
     }
 }
