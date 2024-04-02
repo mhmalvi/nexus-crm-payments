b09d80f70b566e2c5e733d151933dc113f31527b
TanjibQuad <tanjib@quadque.tech>
2024-04-02 08:09:09 +0600

    subscription mail

--- resources/views/subcriptionUpgrade.blade.php
+++ resources/views/subcriptionUpgrade.blade.php
@@ -5,11 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script> --}}
 </head>
 
 <body style="background:#e6e6e6;">
@@ -24,7 +19,7 @@
             <div style="justify-content:center; margin:auto; align-items:center; ">
 
 
-                <img style="max-width:76%;justify-content:center;margin:auto;display:flex;"
+                <img style="max-width:60%;justify-content:center;margin:auto;display:flex;"
                     src="https://crm-payment.queleadscrm.com/assets/thank_you.jpg" />
 
 
@@ -36,7 +31,7 @@
                 <div>
                     <p>Email: {{ $email }}</p>
                     {{-- <p>Email: a@a.com</p> --}}
-                    {{-- <p class="m-auto d-flex justify-content-center" style="font-weight:bold">Thank you for your
+                    {{-- <p class="" style="font-weight:bold">Thank you for your
                         upgrading to
                         fgfd hvfhh subscription.</p> --}}
                         <p class="m-auto d-flex justify-content-center" style="font-weight:bold">Thank you for your upgrading to {{ $package }} {{ $interval.'ly' }} subscription.</p>
@@ -46,7 +41,7 @@
                 </div>
 
             </div><br>
-            <div class="m-auto">
+            <div class="">
 
                 
 
@@ -60,12 +55,12 @@
 
     </div>
 </body>
-<div style="top:100%; left:50%; position:absolute; transform: translate(-50%, 0);">
+{{-- <div style="top:100%; left:50%; position:absolute; transform: translate(-50%, 0);">
 
     <p>© 2024 Quadque Technologies. All Rights
         Reserved.</p>
 
-</div>
+</div> --}}
 
 </html>
 
@@ -79,7 +74,6 @@
         width: 36%;
         position: absolute;
         transform: translate(-50%, -50%);
-        height: 60%;
         top: 50%;
         left: 50%;
 
