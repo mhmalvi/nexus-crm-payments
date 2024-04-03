6421000b79b6617a1000b7093e4c538e0b1561d8
TanjibQuad <tanjib@quadque.tech>
2024-04-03 09:53:40 +0600

    create price

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ use App\Http\Controllers\payway\PaywayController;
 // Route::get('/', function () {
 //     return view('trialPeriod');
 // });
-Route::get('/upgrade', function () {
-return view('subcriptionUpgrade');
-});
+// Route::get('/upgrade', function () {
+// return view('subcriptionUpgrade');
+// });
 Route::get('/payway', [PaywayController::class, 'payway']);
 Route::get('/checkout', [CheckoutPaymentController::class, 'checkout']);
 Route::get('payment/success', [CheckoutPaymentController::class, 'success'])->name('success');
