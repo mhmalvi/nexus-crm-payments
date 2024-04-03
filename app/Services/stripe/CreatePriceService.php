faa340da159b00ed1d12c97407950b55259dd748
TanjibQuad <tanjib@quadque.tech>
2024-04-03 09:34:08 +0600

    create price

--- app/Services/stripe/CreatePriceService.php
+++ app/Services/stripe/CreatePriceService.php
@@ -8,10 +8,30 @@ class CreatePriceService
 {
     public function createPrice($data)
     {
-        $price =
-            Price::where('client_id', $data[4])->orWhere('unit_amount', $data[1])->orWhere('interval', $data[2])->exists();
-        if ($price) {
-            return 422;
+        $prod = Price::where('prod_id', $data[3])->exists();
+        if ($prod) {
+            $price =
+                Price::orWhere('unit_amount', $data[1])->orWhere('interval', $data[2])->exists();
+            if ($price) {
+                return 422;
+            } else {
+                Price::create([
+                    'currency' => $data[0],
+                    'unit_amount' => $data[1],
+                    'recurring' => ['interval' => $data[2]],
+                    'product' => $data[3],
+                    'client_id' => $data[4]
+                ]);
+                $stripe = new
+                    \Stripe\StripeClient(config("app.stripe_secret"));
+                return $stripe->prices->create([
+                    'currency' => $data[0],
+                    'unit_amount' => $data[1],
+                    'recurring' => ['interval' => $data[2]],
+                    'product' => $data[3],
+                    'client_id' => $data[4]
+                ]);
+            }
         } else {
             Price::create([
                 'currency' => $data[0],
