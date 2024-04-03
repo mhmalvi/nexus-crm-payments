803e5f129787823904fda5b2e86d89f9a66db050
TanjibQuad <tanjib@quadque.tech>
2024-04-03 08:47:29 +0600

    create price

--- app/Http/Controllers/price/PriceController.php
+++ app/Http/Controllers/price/PriceController.php
@@ -3,24 +3,36 @@
 namespace App\Http\Controllers\price;
 
 use App\Http\Controllers\Controller;
+use App\Services\stripe\CreatePriceService;
 use Illuminate\Http\Request;
 
 class PriceController extends Controller
 {
+    private $createPrice;
+    public function __construct(CreatePriceService $createPrice)
+    {
+        $this->createPrice = $createPrice;
+    }
     public function createPrice(Request $request)
     {
-        $stripe = new
-            \Stripe\StripeClient(config("app.stripe_secret"));
-        $response = $stripe->prices->create([
-            'currency' => $request->currency,
-            'unit_amount' => $request->unit_amount,
-            'recurring' => ['interval' => $request->interval],
-            'product' => $request->prod_id,
-        ]);
-        return response()->json([
-            'message'=>'inserted',
-            'status'=>201,
-            'data'=>$response
-        ]);
+        $data = [
+            $currency = $request->currency,
+            $unit_amount = $request->unit_amount,
+            $interval = $request->interval,
+            $product = $request->prod_id,
+        ];
+        $response = $this->createPrice->createPrice($data);
+        if ($response) {
+            return response()->json([
+                'message' => 'inserted',
+                'status' => 201,
+                'data' => $response
+            ], 201);
+        } else {
+            return response()->json([
+                'message' => 'Not found',
+                'status' => 404
+            ], 404);
+        }
     }
 }
--- app/Services/stripe/CreatePriceService.php
+++ app/Services/stripe/CreatePriceService.php
@@ -1,8 +1,18 @@
 <?php
+
 namespace App\Services\stripe;
 
-class CreatePriceService{
-    public function CreateService(){
-        
+class CreatePriceService
+{
+    public function createPrice($data)
+    {
+        $stripe = new
+            \Stripe\StripeClient(config("app.stripe_secret"));
+        return $stripe->prices->create([
+            'currency' => $data[0],
+            'unit_amount' => $data[1],
+            'recurring' => ['interval' => $data[2]],
+            'product' => $data[3],
+        ]);
     }
 }
