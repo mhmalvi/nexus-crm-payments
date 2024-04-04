806df6d7ca1ff0cc4ffe222326527553422ada71
TanjibQuad <tanjib@quadque.tech>
2024-04-04 12:48:10 +0600

    trial date fix

--- app/Http/Controllers/subscription/SubscriptionController.php
+++ app/Http/Controllers/subscription/SubscriptionController.php
@@ -161,7 +161,7 @@ class SubscriptionController extends Controller
 
     public function trialCheck(Request $request)
     {
-        dd(Carbon::parse()->toDateTimeString());
+        dd(Carbon::parse()->setTimezone('Asia/Dhaka')->toDateTimeString());
         $company = Company::where('active', 1)->get();
         foreach ($company as $company) {
             if ($company->package == "trial") {
