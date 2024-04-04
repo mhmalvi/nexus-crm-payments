e366aa27915907635129f64072d2a2a6ff186360
TanjibQuad <tanjib@quadque.tech>
2024-04-04 13:54:15 +0600

    trial date fix

--- app/Http/Controllers/subscription/SubscriptionController.php
+++ app/Http/Controllers/subscription/SubscriptionController.php
@@ -163,7 +163,7 @@ class SubscriptionController extends Controller
     public function trialCheck(Request $request)
     {
         $ip = $request->ip();
-        dd($ip);
+        // dd($ip);
         $url = 'http://ip-api.com/json/'.$ip;
         $tz = file_get_contents($url);
         $tz = json_decode($tz,true)['timezone'];
