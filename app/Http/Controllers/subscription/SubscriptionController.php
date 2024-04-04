f7790f4f7c00a86f326e23385057d6289dccf92c
TanjibQuad <tanjib@quadque.tech>
2024-04-04 12:40:20 +0600

    trial date fix

--- app/Http/Controllers/subscription/SubscriptionController.php
+++ app/Http/Controllers/subscription/SubscriptionController.php
@@ -161,7 +161,7 @@ class SubscriptionController extends Controller
 
     public function trialCheck(Request $request)
     {
-        dd(Carbon::now()->date);
+        dd(json_decode(Carbon::now()));
         $company = Company::where('active', 1)->get();
         foreach ($company as $company) {
             if ($company->package == "trial") {
