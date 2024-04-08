22e0ae17bd74f502bdd45622bf2b49cd6171fdc6
TanjibQuad <tanjib@quadque.tech>
2024-04-08 08:47:38 +0600

    trial date fix

--- app/Services/stripe/CreateMonthlySubscriptionService.php
+++ app/Services/stripe/CreateMonthlySubscriptionService.php
@@ -17,14 +17,13 @@ class CreateMonthlySubscriptionService
 
         'items' => [['price' => $data[3]]],
         ]);
-        dd($response->current_period_end);
-        $current_date = Carbon::now();
-            $end_date = $current_date->addDays(1);            
+        // dd($response->current_period_end);
+        $current_date = Carbon::now();           
         $company = Company::where('connect_id',$data[0])->first();
         $company->active = $data[4];
         $company->package = $data[2];
         $company->interval = $data[1];
-        $company->end_date = $end_date;
+        $company->end_date = $response->current_period_end;
         $company->save();
         
         // dd($response);
