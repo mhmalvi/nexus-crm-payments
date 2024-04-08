15575e05edb7ee6814f5eacef477bd1080be9ef2
TanjibQuad <tanjib@quadque.tech>
2024-04-08 13:00:49 +0600

    cancel subscription

--- app/Http/Controllers/subscription/SubscriptionController.php
+++ app/Http/Controllers/subscription/SubscriptionController.php
@@ -127,6 +127,7 @@ class SubscriptionController extends Controller
                     ) {
                         // dd($request->sub_id);                        
                         $response = $this->createSubscriptions->createSubscription($data);
+                        dd($request->sub_id);
                         $this->cancelSubscription->cancelSubscription($request->sub_id);
                         if ($response) {
                             Mail::to($company->business_email)->send(new
