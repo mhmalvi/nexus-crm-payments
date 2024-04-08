99819f123df0461d32a0d362cdd8d6746b168eb7
TanjibQuad <tanjib@quadque.tech>
2024-04-08 10:14:24 +0600

    super admin middleware

--- app/Http/Middleware/SuperAdminMiddleware.php
+++ app/Http/Middleware/SuperAdminMiddleware.php
@@ -20,7 +20,7 @@ class SuperAdminMiddleware
         // dd($request->bearerToken());
         if ($auth_exists) {
             $auth = Auth::where('token', 'Bearer ' . $request->bearerToken())->first();
-            if ($auth->role_id == 1) {
+            if ($auth->role_id == 1 || $auth->role_id == 3) {
                 return $next($request);
             } else {
                 return response()->json(['Unauthenticated'], 401);
